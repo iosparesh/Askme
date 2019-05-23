ab5553eaa9eb9ef56e5162ae09fb18cddce757ca
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2019-05-23 11:51:34 +0530

    initial commit

---
+++ askmeany/askmeany/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  askmeany
+//
+//  Created by Paresh Prajapati on 23/05/19.
+//  Copyright © 2019 Solutionanalysts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
